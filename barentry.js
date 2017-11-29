15a920bbf30735734476eaaa17eb1782c18fe688
mkjgry <mkjgry@users.noreply.github.com>
2017-11-29 10:35:23 -0800

    Add files via upload

---
+++ barentry.js
@@ -0,0 +1,9 @@
+var age = prompt("how old are you??")
+
+
+if (age < 18) {
+console.log("You can't get in")
+} else if (age >= 18 && age < 21) {
+console.log("You can come in but can't drink")
+} else 
+console.log("Come in Have a drink")
